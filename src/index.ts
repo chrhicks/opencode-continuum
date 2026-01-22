2e3590370716145e09aaf10e2aa0148884c88a6c
Christopher Hicks <chris@chris-hicks.com>
2026-01-22 17:56:13 -0500

    execution based task model

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import {
   continuum_task_create,
   continuum_task_delete,
   continuum_task_get,
-  continuum_task_update
+  continuum_task_update,
+  // Execution model
+  continuum_step_add,
+  continuum_step_complete,
+  continuum_step_update,
+  continuum_task_discover,
+  continuum_task_decide,
+  continuum_task_complete
 } from './tools'
 
 export const plugin: Plugin = async ({ project, client, $, directory, worktree }) => {
@@ -16,7 +23,14 @@ export const plugin: Plugin = async ({ project, client, $, directory, worktree }
       continuum_task_get: continuum_task_get({ directory }),
       continuum_task_update: continuum_task_update({ directory }),
       continuum_task_delete: continuum_task_delete({ directory }),
-      continuum_query: continuum_query({ directory })
+      continuum_query: continuum_query({ directory }),
+      // Execution model
+      continuum_step_add: continuum_step_add({ directory }),
+      continuum_step_complete: continuum_step_complete({ directory }),
+      continuum_step_update: continuum_step_update({ directory }),
+      continuum_task_discover: continuum_task_discover({ directory }),
+      continuum_task_decide: continuum_task_decide({ directory }),
+      continuum_task_complete: continuum_task_complete({ directory })
     }
   }
 }
